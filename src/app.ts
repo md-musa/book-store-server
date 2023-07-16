f3145fe61c94287c350ca735599f1818519757e8
md-musa <mohammad.musa706@gmail.com>
2023-07-16 13:42:22 +0600

    cors added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import 'express-async-errors';
 import express, { Application } from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import errorHandler from './app/middlewares/errorHandler';
 import notFoundError from './app/middlewares/notFoundError';
@@ -8,10 +9,11 @@ import notFoundError from './app/middlewares/notFoundError';
 import authRoute from './app/modules/auth/auth.route';
 import bookRoute from './app/modules/book/book.route';
 import wishListRoute from './app/modules/wishlist/wishlist.route';
-import readingList from './app/modules/readingList/readingList.route';
+import readingListRoute from './app/modules/readingList/readingList.route';
 
 const app: Application = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
@@ -20,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/v1/auth', authRoute);
 app.use('/api/v1/books', bookRoute);
 app.use('/api/v1/wishlists', wishListRoute);
-app.use('/api/v1/readingLists', readingList);
+app.use('/api/v1/readingLists', readingListRoute);
 app.get('/', (req, res) => {
   res.send('HELLO WORLD');
 });
